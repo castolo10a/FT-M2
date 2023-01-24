58d626c1b087a3e119fd1c8d4fbb7cb062789fa0
Daniel Castillo <Daniel.302009@hotmail.com>
2023-01-24 00:54:31 -0500

    HW 02 React-intro-integration

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,5 +2,16 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div></div>;
+   return (
+      <div>
+         {characters.map((personaje) => <Card 
+               name={personaje.name}
+               species={personaje.species}
+               gender={personaje.gender}
+               image={personaje.image}
+               onClose={() => window.alert('Emulamos que se cierra la card')}
+            /> 
+         )}
+      </div>
+   );
 }
