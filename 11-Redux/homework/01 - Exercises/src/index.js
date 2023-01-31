e02888bd4608dc98239d5f0836f88bfdde4135b5
Daniel Castillo <Daniel.302009@hotmail.com>
2023-01-31 12:41:37 -0500

    HW 01 REDUX

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -3,10 +3,10 @@ const contador = require("./reducer");
 const { incremento, decremento } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+const store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+const valor = document.getElementById("valor");
 
 // Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
@@ -14,11 +14,29 @@ var valor;
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
+  let counter = store.getState().contador;
+  valor.innerHTML = counter;
 }
 
 // Ejecutamos la función 'renderContador':
+renderContador();
 
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
+store.subscribe(renderContador);
 
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
+let incrementoBtn = document.getElementById('incremento')
+incrementoBtn.addEventListener("click", ()=> store.dispatch(incremento()))
+
+let decrementoBtn = document.getElementById('decremento')
+decrementoBtn.addEventListener("click", () => store.dispatch(decremento()))
+
+let incrementoImpar = document.getElementById('incrementoImpar')
+incrementoImpar.addEventListener("click", ()=> 
+  store.getState().contador % 2 !== 0 && store.dispatch(incremento()))
+
+let incrementoAsync = document.getElementById('incrementoAsync')
+incrementoAsync.addEventListener("click", ()=> setTimeout(()=>{
+  store.dispatch(incremento())
+}, 1000))
