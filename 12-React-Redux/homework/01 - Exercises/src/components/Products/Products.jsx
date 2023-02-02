f709f80d172cac76e63a4ac377554c79502c0e24
Daniel Castillo <Daniel.302009@hotmail.com>
2023-02-01 23:21:49 -0500

    HW 01 REACT-REDUX

--- 12-React-Redux/homework/01 - Exercises/src/components/Products/Products.jsx
+++ 12-React-Redux/homework/01 - Exercises/src/components/Products/Products.jsx
@@ -1,23 +1,49 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import './products.css';
-//El componente Card lo exportamos haciendo destructuring para poder testearlo
-// import Card from '../Card/Card'
+import Card from '../Card/Card';
+import {getStoreName} from '../../redux/actions/actions'
 
-export function Products() {
+export function Products({list, storeName, getStoreName}) {
+
+   React.useEffect(()=>{
+      getStoreName()
+   }, [])
+
    return (
       <>
          <div className='productsBg'>
-            <h1 className='productsTl'>HENRY MARKET</h1>
+            <h1 className='productsTl'>{storeName}</h1>
 
             <div className='productsList'>
-               {/* ¡Renderiza aquí todas tus cards! */}
+               {
+                  list.map((product) => {
+                     return <Card 
+                        name={product.name}
+                        price={product.price}
+                        id={product.id}
+                        key={product.id}
+                     />
+                  })
+               }
             </div>
          </div>
       </>
    );
 }
 
-export function mapStateToProps() {}
+export function mapStateToProps(state) {
+   return {
+      ...state,
+      list: state.list,
+      storeName: state.storeName
+   }
+}
 
-export default connect(mapStateToProps, null)(Products);
+export const mapDispatchToProps = (dispatch) => {
+   return {
+      getStoreName: () => dispatch(getStoreName())
+   }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
---
+++ 12-React-Redux/homework/01 - Exercises/src/redux/actions/actions.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+const { ADD_PRODUCT, DELETE_PRODUCT, GET_STORE_NAME } = require('./types')
+
+export const addProduct = (product) => {
+    return {
+        type: ADD_PRODUCT,
+        payload: product
+    }
+};
+
+export const deleteProduct = (id) => {
+    return {
+        type: DELETE_PRODUCT,
+        payload: id
+    }
+};
+
+export const getStoreName = () => {
+    return async function (dispatch) {
+        let response = await axios.get("http://localhost:3001/store");
+        return dispatch({
+            type: GET_STORE_NAME,
+            payload: response.data.name
+        }) 
+    }
+};
+
