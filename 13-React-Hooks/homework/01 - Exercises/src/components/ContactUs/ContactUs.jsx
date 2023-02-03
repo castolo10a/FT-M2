13f6530984e47194c2e675dc11ed5e57fc303ed5
Daniel Castillo <Daniel.302009@hotmail.com>
2023-02-03 10:47:59 -0500

    HW 01 REACT-HOOKS

--- 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
+++ 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
@@ -1,13 +1,41 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { enviarForm }  from "../../redux/actions/actions";
 
 const ContactUs = () => {
+  const dispatch = useDispatch();
+
+
+  const [form, setForm] = React.useState({
+    nombre: "",
+    email: "",
+    asunto: "",
+    mensaje: ""
+  })
+
+  const handleInput = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value
+    })
+  }
+  const handleSubmit = () => {
+    dispatch(enviarForm(form))
+    setForm({
+      nombre: "",
+      email: "",
+      asunto: "",
+      mensaje: ""
+    })
+  }
+
   return (
     <div className="contactBg">
-      <input name="nombre"></input>
-      <input name="email"></input>
-      <input name="asunto"></input>
-      <input name="mensaje"></input>
-      <button>ENVIAR</button>
+      <input name="nombre" onChange={handleInput} value={form.name}></input>
+      <input name="email" onChange={handleInput} value={form.email}></input>
+      <input name="asunto" onChange={handleInput} value={form.asunto}></input>
+      <input name="mensaje" onChange={handleInput} value={form.mensaje}></input>
+      <button onClick={handleSubmit}>ENVIAR</button>
     </div>
   );
 };
--- 13-React-Hooks/homework/01 - Exercises/src/redux/reducer/reducer.js
+++ 13-React-Hooks/homework/01 - Exercises/src/redux/reducer/reducer.js
@@ -2,6 +2,17 @@ const initialState = {
   formulario: {},
 };
 
-const rootReducer = (state = initialState, { type, payload }) => {};
+const rootReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "FORM_DATA":
+      return {
+        ...state,
+        formulario: payload
+      }
+      
+    default:
+      return{...state};
+  }
+};
 
 export default rootReducer;
